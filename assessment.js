24ec662b6530db0a5737e8e25c2469d09271b05c
yakan-007 <38965953+yakan-007@users.noreply.github.com>
2018-05-04 11:55:31 +0900

    #1 に対する対応を行いました。

--- assessment.js
+++ assessment.js
@@ -41,6 +41,7 @@
         anchor.className = 'twitter-hashtag-button';
         anchor.innerText = 'Tweet #%E3%81%82%E3%81%AA%E3%81%9F%E3%81%AE%E3%81%84%E3%81%84%E3%81%A8%E3%81%93%E3%82%8D';
         tweetDivided.appendChild(anchor);
+        
 
         twttr.widgets.load();
     };
